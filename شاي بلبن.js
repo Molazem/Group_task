58c521a27d7d0bd27030f93bfc5ee6a1da4475f3
Molazem <abdallah.molz.8525@gmail.com>
2023-08-15 20:19:48 +0300

    lsa

---
+++ شاي بلبن.js
@@ -0,0 +1,7 @@
+let count = 1
+document.getElementById("btn").onclick = function (){
+    document.getElementById("mbet").innerHTML = " عدد مرات التوجل "+count+" مره";
+    count ++;
+    console.log("شغال");
+}
+
